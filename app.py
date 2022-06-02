c72a52c1bbf7d0d6e4eafb4340a1140df5e2785f
Guy Marshall <guymarshall.uk@gmail.com>
2022-06-02 16:01:48 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+# sentence = "This is a common interview question"
+
+filename = input("Enter a filename to scan: ")
+file = open(filename, "r")
+sentence = file.read()
+
+char_frequency = {}
+
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+
+# pprint(char_frequency, width=1)
+
+char_frequency_sorted = sorted(char_frequency.items(),
+                               key=lambda kv: kv[1],
+                               reverse=True)
+print(char_frequency_sorted[0])
